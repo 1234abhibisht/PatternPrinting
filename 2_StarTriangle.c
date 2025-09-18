867d0e74d2e0e3498757cb4e3ab7316156bd5694
1234abhibisht <abhibisht129@gmail.com>
2025-09-18 14:31:10 +0530

    Create 2_StarTriangle.c

---
+++ 2_StarTriangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int triangle(int n) {
+    for(int i = 1; i <= n; i++) {  /* horizontal rows */
+        for(int j =1; j <= i; j++) {  /* vertical columns */
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of rows : ");
+    scanf("%d",&n);
+    triangle(n);
+    return 0;
+}
